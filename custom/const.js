8ac7728c093941c3db5b91bb56c3c0bda0ef4224
dinhtrivonguyen <dinhtrivonguyen@gmail.com>
2020-08-25 09:31:50 +0700

    first commit

---
+++ custom/const.js
@@ -0,0 +1,34 @@
+module.exports = {
+	DEBUG : false,
+
+	//FACEBOOK TOKEN
+	FB_APP_SECRET : 'df51031565cc73a31ce237afe0d7284c',
+	FB_PAGE_VERIFY_TOKEN : 'passsword_dang_yeu_nhat_qua_dat', // đặt 1 mã bất kỳ
+	FB_PAGE_ACCESS_TOKEN : 'EAACE3JTDalABAPBd0cXoAxDizJAejEQ8jUZCMZB3WU4zR5l46AGNMYaSj4nwKos7UO1hk8mKlYaWBcNJFnFc8a5mSC8CYNxmAYlEKh2PIswNaVA1WVO9vdG7j681DyH51fluG4UTXWxNa00xqDgWDtOtZCNzFZAscZAvDDfBCXCdLxoUPFDpoQ03XQZCFLNxsZD',
+
+	//HEROKU STUFFS
+	APP_NAME : '',
+	HEROKU_API_KEY : '',
+	KEEP_APP_ALWAYS_ON : false, // đổi thành true nếu đã thêm credit card vào heroku
+
+	//MONGODB SETUP
+	DB_CONFIG_URI : 'mongodb://Neo:NeoTheThird1404%5E%5E@cluster0-shard-00-00-3y5vp.gcp.mongodb.net:27017,cluster0-shard-00-01-3y5vp.gcp.mongodb.net:27017,cluster0-shard-00-02-3y5vp.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+
+	//ANALYTICS
+	HAS_POST_LOG : false,
+	POST_LOG_ID : '',
+	POST_LOG_ENTRY1 : '',
+	POST_LOG_ENTRY2 : '',
+
+	//GOOGLE FORMS
+	REPORT_LINK : "http://bit.ly/yourfeedbackaboutme",
+
+	//OTHERS
+	//(không bắt buộc) Cách bật tính năng hiện đã xem (seen): https://goo.gl/xjw9nP
+	MAX_PEOPLE_WAITROOM : 7, //Số người tối đa trong phòng chờ
+	MAX_WAIT_TIME_MINUTES : 60, //Số phút tối đa 1 người đc phép trong phòng chờ.
+	                            //Đặt 0 để cho phép đợi bao lâu cũng đc
+
+	//ADMIN UI
+	ADMIN_PASSWORD : "@1234567890" //password để login vào trang admin
+};
---
+++ custom/lang.js
@@ -0,0 +1,49 @@
+
+// LƯU Ý: MỖI LẦN CHỈNH SỬA FILE NÀY, BẠN CẦN CHẠY DEPLOY.BAT ĐỂ UPDATE LÊN SERVER
+
+module.exports = {
+	BAO_TRI : "Hệ thống hiện đang bảo trì. Chúng tớ sẽ sớm kết nối lại và thông báo cho các bạn.",
+	FIRST_COME : 'Chào mừng đến với PCT Strangers", "Bạn đã sẵn sàng để kết nối với người ấy? Hãy nhập một ký tự bất kỳ để bắt đầu!',
+	HUONG_DAN : "🤗 Nhập từ khóa batdau hoặc lựa chọn giới tính trong menu để bắt đầu tìm kiếm người ấy cho mình nhé.",
+	BATDAU_OKAY : "\nChúng tớ đang tìm người phù hợp với bạn nhất...",
+	BATDAU_WARN_GENDER : "Thông báo: Bạn không chọn giới tính. Chúng tớ sẽ ghép cặp ngẫu nhiên cho bạn. ",
+	BATDAU_ERR_ALREADY : "Hãy chắc chắn trước khi ketthuc và gõ batdau để tìm người mới nhé <3",
+	WAITING : "\nChúng tớ đang tìm người phù hợp với bạn nhất",
+	START_CHAT : "💞 Bạn đã được kết nối với một bạn cùng trường! Gõ ketthuc để kết thúc cuộc trò chuyện nhé. Chúc hai bạn trò chuyện vui vẻ  :)",
+	END_CHAT : "\nGửi batdau để ghép cặp với người mới nhé!",
+	END_CHAT_FORCE : "Hiện tại chúng tớ không tìm được ai cả. Bạn hãy thử lại nha :<",
+	ERR_UNKNOWN : "Hệ thống xảy ra lỗi nhưng ko nghiêm trọng lắm\nHãy gửi ketthuc để thoát ra và thử lại",
+	ERR_ATTACHMENT : "Lỗi: chúng mình chưa hỗ trợ gửi dạng dữ liệu này",
+	ATTACHMENT_FILE : " Bạn ý đã gửi 1 tệp tin: ",
+	ATTACHMENT_LINK : " Bạn ý đã gửi 1 đường link: ",
+	ERR_FAKE_MSG : " Lỗi: Bạn không được giả mạo tin nhắn của bot",
+	SQL_ERR : " Lỗi: Không thể kết nối với database. Hãy báo cho admin!",
+	GENDER_ERR : " Lỗi: Giới tính nhập vào không hợp lệ!",
+	GENDER_WRITE_OK : " 🎉 Chúng tớ sẽ tìm người ấy theo lựa chọn của bạn là: ",
+	GENDER_WRITE_WARN : "\n\nLưu ý: Lựa chọn này chỉ có tác dụng với phần lớn các cuộc nói chuyện.",
+	GENDER_ARR : ['không quan tâm', 'nam', 'nữ'],
+	HELP_TXT : " 📩 Hướng dẫn sử dụng các câu lệnh để tìm đối phương:\n" +
+				"- batdau: Bắt đầu tìm kiếm \n"+
+				"- ketthuc: Kết thúc cuộc trò chuyện\n"+
+				"- trogiup: Xem hướng dẫn\n"+
+				"\nCác câu lệnh có thể dùng khi đang không kết nối với ai:\n"+
+				"- tim nu: Tìm con gái\n"+
+				"- tim nam: Tìm bạn trai \n"+
+				"\n Phan Chau Trinh Strangers ver01\n"+
+				"\n Sự phát triển hệ thống thuộc về Coreteam Gen4",
+				
+	BROADCAST_TXT : "this is a message from admin",			
+
+	KEYWORD_BATDAU : 'batdau',
+	KEYWORD_KETTHUC : 'ketthuc',
+	KEYWORD_GENDER : 'tim',
+	KEYWORD_HELP : 'trogiup',
+	KEYWORD_TIEUCHI : 'tieuchi',
+	KEYWORD_BROADCAST : 'broadcastmsg',
+	KEYWORD_CAT : 'meow',
+	KEYWORD_DOG : 'gauw',
+
+	ERR_200: '[Chatbot] Bạn chat không thể nhận tin nhắn do đã xóa inbox hoặc block page.',
+	ERR_10: '[Chatbot] Bạn chat không thể nhận tin nhắn do 2 bạn không nói chuyện gì trong vòng 24h. Gửi ketthuc để kết thúc chat.',
+	ERR_TOO_LONG: '[Chatbot] Lỗi: tin nhắn quá dài (nhiều hơn 640 ký tự). Hãy chia nhỏ tin nhắn và gửi dần.'
+};
